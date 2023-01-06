bb07049cbe5c04568c7a42cb67dcc7cddc982102
Tyler Hancock <tyler.hancock13@hotmail.com>
2023-01-05 21:07:08 -0400

    Initial Commit

---
+++ weather.js
@@ -0,0 +1,93 @@
+window.onload = function() {
+    document.getElementById("forecast").style.display = 'none';
+}
+
+let getWeatherForLocation = async () => {
+
+    document.getElementById("forecast").style.display = 'block';
+
+    let city = document.getElementById('location').value;
+
+    const request = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c55b5e9d4864e69d994e516d95e4ffb3`)
+
+    let data = await request.json();
+    console.log(data);
+
+    var d = new Date(data.sys.sunrise*1000);
+    console.log(d);
+
+    // ---------------------- CALCULATE LOCAL TIME ZONE ----------
+    console.log(data.timezone);
+    const secondsFromUTC = data.timezone;
+
+    function subtractHours(date, hours){
+        date.setHours(date.getHours() - hours);
+
+        return date;
+    }
+    
+    const utcDate = new Date().toUTCString();
+    dateFormat = new Date(Date.parse(utcDate));
+    
+
+    const newDate = subtractHours(dateFormat, 6);
+
+    console.log(newDate);
+    // ------------------- END OF CALCULATE TIME ZONE --------------
+    let cityName = document.getElementById('cityName');
+    cityName.innerText = data.name.toString();
+    //populateCityForecast(null, data);
+    let tempCelsius = data.main.temp - 273.15;
+    let currentWeather = data.weather[0].description;
+    let weatherIcon = data.weather[0].icon;
+    tempCelsius = Math.round(tempCelsius);
+
+    let windSpeed = setWindSpeed(data);
+    let humidity = setHumidity(data);
+    let pressure = setPressure(data);
+    let sunrise = calculateSunrise(data);
+
+    let feelsLike = data.main.feels_like - 273.15;
+    feelsLike = Math.round(feelsLike);
+    setImageIcon(weatherIcon);
+
+    document.getElementById('cityName').innerHTML = `${data.name.toString()}, ${data.sys.country.toString()}`;
+    document.getElementById('cityTemperature').innerHTML = `${tempCelsius.toString()}°C`;
+    document.getElementById('wind-speed').innerHTML = `${windSpeed.toString()} km/h`;
+    document.getElementById('humidity').innerHTML = `${humidity.toString()}%`;
+    document.getElementById('pressure').innerHTML = `${pressure.toString()} mb`;
+};
+
+function setImageIcon(icon){
+    document.getElementById('weather-icon').src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+}
+
+function populateCityForecast(params) {
+    let cityName = document.getElementById('#cityName');
+    cityName.innerText = params.name.toString();
+}
+
+function setWindSpeed(data){
+    let windSpeed = data.wind.speed * 3.6;
+    windSpeed = Math.round(windSpeed);
+
+    return windSpeed;
+}
+
+function setHumidity(data){
+    let humidity = data.main.humidity;
+
+    return humidity;
+}
+
+function setPressure(data) {
+    let pressure = data.main.pressure;
+
+    return pressure;
+}
+
+function calculateSunrise(data) {
+    let utcTime = Date.now();
+}
+
+document.querySelector('#submitLocation').addEventListener('click', getWeatherForLocation);
