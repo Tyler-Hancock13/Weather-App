e4aaadf83661d062f231f08b6eb2a083298e5cb3
Tyler Hancock <tyler.hancock13@hotmail.com>
2023-05-31 04:15:24 -0300

    Add font and update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,20 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'inter': 'Inter',
+    },
     extend: {
       colors: {
         'aero': '#42BFDD',
         'anti-flash-white': '#F0F6F6',
         'mint-green': '#BBE6E4',
         'indigo-blue': '#084B83',
+        'back-theme': '#f4fbfd',
+        'card-theme': '#d9f2f8',
+        'dark-border': '#156579',
+        'active-theme': '#156579',
+        'header-theme': '#1a7a92',
       },
     },
   },
